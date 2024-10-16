d96af6a908370a95fb5fa9e93c83b894cb207f77
b-hexsoul <brandon.hexsel@gmail.com>
2024-10-16 11:43:36 -0700

    init postgres with prisma migration

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({
+      where,
+    })
+  }
+}
